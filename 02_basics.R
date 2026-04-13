8ec904ffd62bfbc07c5b49fa173384d3011143bc
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2026-04-13 09:34:13 +0530

    Using Print and Declaring Variable

---
+++ 02_basics.R
@@ -0,0 +1,21 @@
+print("Hello World")
+demo<-"Hello R Programmers"
+demo
+print(demo)
+sum<-10+56
+sum
+sum1<-sum+10
+sum1
+#variable naming
+var_name=10
+.var.name=254
+#_var.name=100
+#1varname=258
+#Assignmen of Variable
+var1<-1000
+var1
+var2=2000
+10->var3
+var3
+print(var1,var2,var3)
+cat(var1,var2,var3)
