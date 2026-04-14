f21c82ad0ddc3cf05c0a6357cd36d574ceafcd3b
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2026-04-14 09:52:16 +0530

    Datatypes and class and typeof

---
+++ 03_Basics.R
@@ -0,0 +1,37 @@
+#Datatypes
+#1 Numeric
+num1=10
+num2=1025.45
+num3=-25
+num4=-26.5478
+class(num1)
+class(num4)
+class(num3)
+
+#2 Integer
+int1=10L
+int2=12.25L
+class(int1)
+class(int2)
+typeof(int1)
+typeof(int2)
+
+#3 Complex
+comp1=10+5i
+class(comp1)
+comp2=0+0i
+class(comp2)
+typeof(comp2)
+
+#4 Logical
+logi1=TRUE
+logi2=FALSE
+class(logi1)
+typeof(logi2)
+
+#5 character
+chr1='a'
+class(chr1)
+chr2="Hello World"
+class(chr2)
+typeof(chr2)
