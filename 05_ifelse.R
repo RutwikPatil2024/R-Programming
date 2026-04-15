999b65bfaf74a205cadf47cd57b5b677b859d5a0
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2026-04-15 10:19:27 +0530

    If else if else in R

---
+++ 05_ifelse.R
@@ -0,0 +1,24 @@
+x=100L
+if(is.integer(x)){
+  print("X is integer")
+}else{
+  print("x is not integer")
+}
+
+v<-c("hardwork","is","key","to","success")
+if("key"%in%v){
+  print("Yes")
+}else{
+  print("No")
+}
+
+marks<-98
+if(marks>90){
+  print("A")
+}else if(marks>80){
+  print("B")
+}else if(marks>70){
+  print("C")
+}else if(marks>60){
+  print("D")
+}
